ac01c4741c847d776545b0fd6a1b218eada9bdfa
Priscila Silva <Priscila Silva>
2017-05-07 13:11:27 -0300

    Scripts

---
+++ Assets/Standard Assets/2D/Scripts/respaw.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var contMaxInim : int;
+var inimigo : GameObject;
+var posicaoInimigo : Vector3;
+function Start () {
+	contMaxInim = 0;
+}
+
+function Update () {
+	/*if(contMaxInim<20){
+	posicaoInimigo.x = corpo.posicaoHeroi.x + (Random.value*10)+10;
+	posicaoInimigo.z = corpo.posicaoHeroi.z + (Random.value*10)+10;
+	posicaoInimigo.y = corpo.posicaoHeroi.y + 10;
+
+	Instantiate(inimigo, posicaoInimigo, transform.rotation);
+	contMaxInim = contMaxInim + 1;
+	}*/
+	
+}
